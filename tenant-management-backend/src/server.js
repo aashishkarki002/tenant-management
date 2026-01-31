f51624b915813fd7e966e402623c55b0946375bb
aashishkarki002 <karkiaashish6002@gmail.com>
2026-01-31 19:23:58 +0545

    prod variable

--- tenant-management-backend/src/server.js
+++ tenant-management-backend/src/server.js
@@ -1,12 +1,17 @@
 import app from "./app.js";
 import http from "http";
 import { initializeSocket } from "./config/socket.js";
-import dotenv from "dotenv";
 
-dotenv.config();
+// Load dotenv ONLY in local/dev
+if (process.env.NODE_ENV !== "production") {
+  const dotenv = await import("dotenv");
+  dotenv.config();
+}
+
 const PORT = process.env.PORT || 3000;
+
 const server = http.createServer(app);
-const io = initializeSocket(server);
+initializeSocket(server);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
